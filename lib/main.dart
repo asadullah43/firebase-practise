1d485d6e8d944a40e0d605ec9dda89024a74e0d0
asadullah43 <100843918+asadullah43@users.noreply.github.com>
2023-02-08 20:21:32 +0300

    login screen design

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
       home: SplashScreen(),
     );
   }
--- lib/ui/auth/login_screen.dart
+++ lib/ui/auth/login_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_practise/ui/widget/round_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -8,6 +10,16 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final _formkey = GlobalKey<FormState>();
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,8 +27,65 @@ class _LoginScreenState extends State<LoginScreen> {
         title: const Center(child: Text('Login Screen')),
         automaticallyImplyLeading: false,
       ),
-      body: Column(
-        children: const [],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Form(
+                key: _formkey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      controller: emailController,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter your email',
+                        labelText: 'Email',
+                        prefixIcon: Icon(Icons.email_rounded),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Enter email';
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextFormField(
+                      keyboardType: TextInputType.text,
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter your password',
+                        labelText: 'Password',
+                        prefixIcon: Icon(Icons.lock),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Enter password';
+                        } else {
+                          return null;
+                        }
+                      },
+                    )
+                  ],
+                )),
+            const SizedBox(
+              height: 50,
+            ),
+            RoundButton(
+              title: 'Login',
+              onTap: () {
+                if (_formkey.currentState!.validate()) {}
+              },
+            )
+          ],
+        ),
       ),
     );
   }
