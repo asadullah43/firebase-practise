eb50610021e76082066f321120b1d0ffa42223c6
asadullah43 <100843918+asadullah43@users.noreply.github.com>
2023-02-11 21:19:08 +0300

    fetch data from firebase

--- lib/ui/posts/posts_screen.dart
+++ lib/ui/posts/posts_screen.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
-
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:firebase_practise/ui/auth/login_screen.dart';
 import 'package:firebase_practise/ui/posts/add_post.dart';
 import 'package:firebase_practise/utils/utils.dart';
@@ -16,6 +17,7 @@ class _PostScreenState extends State<PostScreen> {
   @override
   Widget build(BuildContext context) {
     final auth = FirebaseAuth.instance;
+    final ref = FirebaseDatabase.instance.ref('Post');
     return Scaffold(
       appBar: AppBar(
         title: const Text('Post screen'),
@@ -44,6 +46,45 @@ class _PostScreenState extends State<PostScreen> {
               MaterialPageRoute(builder: (context) => const AddPost()));
         }),
         child: const Icon(Icons.add),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child: StreamBuilder(
+            stream: ref.onValue,
+            builder: (context, AsyncSnapshot<DatabaseEvent> snapshot) {
+              if (!snapshot.hasData) {
+                const CircularProgressIndicator();
+              } else {
+                Map<dynamic, dynamic> map =
+                    snapshot.data!.snapshot.value as dynamic;
+                List<dynamic> list = [];
+                list.clear();
+                list = map.values.toList();
+                return ListView.builder(
+                  itemCount: snapshot.data!.snapshot.children.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(list[index]['title']),
+                      subtitle: Text(list[index]['id']),
+                    );
+                  },
+                );
+              }
+              return Text('');
+            },
+          )),
+          Expanded(
+            child: FirebaseAnimatedList(
+                defaultChild: const Text('loading'),
+                query: ref,
+                itemBuilder: (context, snapshot, animation, index) {
+                  return ListTile(
+                    title: Text(snapshot.child('title').value.toString()),
+                  );
+                }),
+          )
+        ],
       ),
     );
   }
